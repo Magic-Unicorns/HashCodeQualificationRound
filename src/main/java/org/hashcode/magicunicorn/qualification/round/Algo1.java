db64636d9771244de4685bf5556de71e632029a7
Isadora Belleville <isadora.belleville@magillem.com>
2020-02-20 20:02:37 +0100

    first algo bis

---
+++ src/main/java/org/hashcode/magicunicorn/qualification/round/Algo1.java
@@ -0,0 +1,33 @@
+package org.hashcode.magicunicorn.qualification.round;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Algo1 {
+
+    int tempsRestant;
+
+    List<Book> nonScannesBooks;
+
+    int scoreMax;
+
+    List<Library> nonInitLibs;
+
+    public Algo1(Data data) {
+        this.tempsRestant = data.getNumberOfDay();
+        this.nonScannesBooks = data.getBooks();
+        scoreMax = 0;
+        this.nonInitLibs = data.getLibraries();
+    }
+
+    public void runAlgo() {
+        while (tempsRestant > 0) {
+            nonInitLibs.sort((l1, l2) -> -Integer.compare(l1.calculateLibScore(tempsRestant, nonScannesBooks),
+                    l2.calculateLibScore(tempsRestant, nonScannesBooks)));
+            Library l = nonInitLibs.get(0);
+            scoreMax += l.calculateLibScore(tempsRestant, nonScannesBooks);
+            nonInitLibs.remove(l);
+            tempsRestant -= l.getSignTime();
+        }
+    }
+}
